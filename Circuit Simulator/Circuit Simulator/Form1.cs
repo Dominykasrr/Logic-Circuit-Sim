a435162f6e7e5ba17016d2e5edc062a88a31d0ac
Dominykas <dominykasrr@gmail.com>
2014-12-09 22:55:27 +0100

    Init Commit

---
+++ Circuit Simulator/Circuit Simulator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circuit_Simulator
+{
+    public partial class Form1 : Form
+    {
+        List<int> i = new List<int>();
+        int e;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            // I wanna find a point
+        void makeConnection()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+        }
+        private void OnMouseClick(MouseEventArgs e) 
+        {
+             FindConnection(e.X, e.Y);
+            
+        }
+        private void FindConnection(int X,int Y)
+        {
+
+        }
+        
+    }
+}
