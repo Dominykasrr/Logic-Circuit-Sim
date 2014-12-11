dbd8b72fa560ea013b5958a79fe555969967d81f
Wéber Márton <webermarci@Webers-MacBook-Pro.local>
2014-12-12 00:52:08 +0100

    Source change
    Removed the second OutputPoint
    Comments are added

--- Circuit Simulator/CircuitSimLib/GeneratedCode/Source.cs
+++ Circuit Simulator/CircuitSimLib/GeneratedCode/Source.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 public class Source : Element
 {
+    //A boolean that states the current output of the source
 	public virtual bool state { get; set; }
+    
+    //An OutputPoint that specifies the output for the source
 	public virtual OutputPoint output { get; set; }
-	public virtual OutputPoint OutputPoint { get; set; }
-
 }
 
