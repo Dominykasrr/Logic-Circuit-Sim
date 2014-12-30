b950f27113cc106b4711747f8e8425e394fa7c67
Wéber Márton <webermarton@gmail.com>
2014-12-30 16:05:05 +0100

    ConnectionPoint edited
    Constuctor added
    AreYouClicked() implemented

--- Circuit Simulator/CircuitSimLib/GeneratedCode/ConnectionPoint.cs
+++ Circuit Simulator/CircuitSimLib/GeneratedCode/ConnectionPoint.cs
@@ -5,13 +5,41 @@ using System.Text;
 
 public abstract class ConnectionPoint
 {
-	public virtual int X { get; set; }
-	public virtual int Y { get; set; }
-	public virtual int Radius { get; set; }
+    /// <summary>
+    /// X coordinate of the ConnectionPoint
+    /// </summary>
+	private int X { get; set; }
 
-	public virtual bool AreYouClicked()
+    /// <summary>
+    /// X coordinate of the ConnectionPoint
+    /// </summary>
+	private int Y { get; set; }
+
+    /// <summary>
+    /// Radius of the ConnectionPoint
+    /// </summary>
+	private int Radius { get; set; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public ConnectionPoint(int x, int y, int r)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Radius = r;
+    }
+
+    /// <summary>
+    /// Checks if this ConnectionPoint was clicked
+    /// </summary>
+    /// <returns>Returns a boolean</returns>
+	public bool AreYouClicked(int x, int y)
 	{
-		throw new System.NotImplementedException();
+        double d = Math.Sqrt(Math.Pow(x-this.X,2) + Math.Pow(y-this.Y,2));
+
+        if (d > this.Radius) return false;
+        else return true;
 	}
 
 }
