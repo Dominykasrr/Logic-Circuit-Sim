dd3acd98b9ee5e0a9fee63491bc0546bf44eee1b
Karuktist <dragan95@abv.bg>
2015-01-06 16:22:10 +0100

    Added a NOT Gate class

---
+++ Circuit Simulator/CircuitSimLib/NotGate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+/// <summary>
+/// An NOT gate class to store the correct image and 
+/// calculate the appropriate output
+/// </summary>
+public class NotGate : Gate
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public NotGate()
+    {
+        this.Input = new InputPoint[1];
+        this.Input[0] = new InputPoint(this.X, this.Y, 10, this);
+        
+
+        this.Output = new OutputPoint(this.X, this.Y, 10, this);
+
+        this.Img[0] = CircuitSimLib.Properties.Resources.inverter;
+    }
+
+    /// <summary>
+    /// Calculates the output for an NOT gate
+    /// </summary>
+    /// <returns>Returns a calculated boolean</returns>
+    public override bool CalculateOutput()
+    {
+        return !(Input[0].ConnectedElementsOutput());
+    }
+}
+
