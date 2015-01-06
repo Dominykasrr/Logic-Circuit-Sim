4238b88021fec5d8e9961aadcc6d43da2d951271
Karuktist <dragan95@abv.bg>
2015-01-06 16:24:23 +0100

    Added a NOT And Gate

---
+++ Circuit Simulator/CircuitSimLib/NAND.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+/// <summary>
+/// An NAND gate class to store the correct image and 
+/// calculate the appropriate output
+/// </summary>
+public class NAndGate : Gate
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public NAndGate()
+    {
+        this.Input = new InputPoint[2];
+        this.Input[0] = new InputPoint(this.X, this.Y, 10, this);
+        this.Input[1] = new InputPoint(this.X, this.Y, 10, this);
+
+        this.Output = new OutputPoint(this.X, this.Y, 10, this);
+
+        this.Img[0] = CircuitSimLib.Properties.Resources.nand;
+    }
+
+    /// <summary>
+    /// Calculates the output for an NAND gate
+    /// </summary>
+    /// <returns>Returns a calculated int</returns>
+    public override bool CalculateOutput()
+    {
+        return !(Input[0].ConnectedElementsOutput() && Input[1].ConnectedElementsOutput());
+    }
+}
+
