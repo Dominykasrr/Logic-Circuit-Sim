b456ab45938936a37860984c7bd16c686bd2c7d2
Karuktist <dragan95@abv.bg>
2015-01-06 16:23:06 +0100

    Added a NOT OR Gate

---
+++ Circuit Simulator/CircuitSimLib/NORGate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+/// <summary>
+/// An NOR gate class to store the correct image and 
+/// calculate the appropriate output
+/// </summary>
+public class NORGate : Gate
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public NORGate()
+    {
+        this.Input = new InputPoint[2];
+        this.Input[0] = new InputPoint(this.X, this.Y, 10, this);
+        this.Input[1] = new InputPoint(this.X, this.Y, 10, this);
+
+        this.Output = new OutputPoint(this.X, this.Y, 10, this);
+
+        this.Img[0] = CircuitSimLib.Properties.Resources.nor;
+    }
+
+    /// <summary>
+    /// Calculates the output for an NOR gate
+    /// </summary>
+    /// <returns>Returns a calculated boolean</returns>
+    public override bool CalculateOutput()
+    {
+        return !(Input[0].ConnectedElementsOutput() || Input[1].ConnectedElementsOutput());
+    }
+}
+
