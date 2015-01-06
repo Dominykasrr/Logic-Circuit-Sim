5df41ed3213eb3b71fcc34f8a8361378f5520ea2
Karuktist <dragan95@abv.bg>
2015-01-06 16:25:00 +0100

    Added a XOR Gate

---
+++ Circuit Simulator/CircuitSimLib/XOR.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+/// <summary>
+/// An XOR gate class to store the correct image and 
+/// calculate the appropriate output
+/// </summary>
+public class XORGate : Gate
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public XORGate()
+    {
+        this.Input = new InputPoint[2];
+        this.Input[0] = new InputPoint(this.X, this.Y, 10, this);
+        this.Input[1] = new InputPoint(this.X, this.Y, 10, this);
+
+        this.Output = new OutputPoint(this.X, this.Y, 10, this);
+
+        this.Img[0] = CircuitSimLib.Properties.Resources.or;
+    }
+
+    /// <summary>
+    /// Calculates the output for an XOR gate
+    /// </summary>
+    /// <returns>Returns a calculated boolean</returns>
+    public override bool CalculateOutput()
+    {
+        return Input[0].ConnectedElementsOutput() ^ Input[1].ConnectedElementsOutput();
+    }
+}
+
